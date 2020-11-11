6768128330b0029ef73269c59b70f85fd8e82736
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-11 12:28:24 +0100

    Adding a new to-do item when clicking a button

--- action.js
+++ action.js
@@ -1,7 +1,9 @@
 /*ASSIGNMENT
 Definire una lista di to-do
 e stamparli in pagina in un elenco.
-Predisporre un input testuale per aggiungere un nuovo to-do: al click di un pulsante, il testo inserito nell'input viene aggiunto alla lista dei to-do.
+Predisporre un input testuale per aggiungere un nuovo to-do:
+al click di un pulsante,
+il testo inserito nell'input viene aggiunto alla lista dei to-do.
 Al click su un item della lista, il to-do viene rimosso.
 BONUS: intercettare anche il tasto "ENTER" per aggiungere un nuovo to-do.
 */
@@ -18,8 +20,14 @@ let app = new Vue({
       'Seeing the Pyramids of Egypt',
       'Walking the Camino de Santiago',
     ],
+    new_todo: '',
   },  // Closing "data"
   methods: {
-
+    addTodo: function() {
+      this.todo_list.push(this.new_todo);
+      // Output in console
+      console.log('The updated to-do list is: ' , this.todo_list);
+      console.log(`The new to-do item is: "${this.new_todo}".`);
+    }
   },  // Closing "methods"
 });
